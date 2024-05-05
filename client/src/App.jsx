ffed0949e2bf52aa379fe78f558cf879c7e8042a
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-05 11:14:12 -0400

    established files

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import BucketList from "./components/Home";
+
+function App() {
+  return (
+    <div className="bucket-app">
+      <BucketList />
+    </div>
+  );
+}
+
+export default App;
