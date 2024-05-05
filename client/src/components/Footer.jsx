828dd06a751ca5f6f0a5dcb296cf1fbe03c7359c
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-05 12:55:41 -0400

    slight updates to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,20 +1,12 @@
 const Footer = () => {
   return (
     <footer>
-      <a
-        href="https://github.com/vpontual"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        GitHub
-      </a>
-      <a
-        href="https://linkedin.com/in/vitorpontual"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        LinkedIn
-      </a>
+      <div>
+        <a href="https://github.com/vpontual">GitHub</a>
+      </div>
+      <div>
+        <a href="https://linkedin.com/in/vitorpontual">LinkedIn</a>
+      </div>
     </footer>
   );
 };
