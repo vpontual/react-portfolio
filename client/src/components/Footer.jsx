0ac5fb400ec5d7be7b407bd97b07d6bfd3f5c032
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-05 12:14:39 -0400

    wrote the footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,25 +2,18 @@ const Footer = () => {
   return (
     <footer>
       <a
-        href="https://github.com/your-username"
+        href="https://github.com/vpontual"
         target="_blank"
         rel="noopener noreferrer"
       >
         GitHub
       </a>
       <a
-        href="https://linkedin.com/in/your-username"
+        href="https://linkedin.com/in/vitorpontual"
         target="_blank"
         rel="noopener noreferrer"
       >
         LinkedIn
-      </a>
-      <a
-        href="https://twitter.com/your-username"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Twitter
       </a>
     </footer>
   );
