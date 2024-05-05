ae308c8e86b52574732cd39320c9693bfaee1fe3
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-05 16:30:59 -0400

    added resume text and sample file for download

--- client/src/components/Resume.jsx
+++ client/src/components/Resume.jsx
@@ -2,13 +2,9 @@ const Resume = () => {
   return (
     <section>
       <h2>Resume</h2>
-      <p>Hello! Welcome to my Resume section</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque iusto
-        autem ullam quibusdam quas earum dolorem odio aliquam, cupiditate, natus
-        temporibus molestiae illo, reiciendis corporis vero iste expedita nulla
-        labore.
-      </p>
+      <a href="./public/documents/resume.txt" download>
+        Download Resume
+      </a>
     </section>
   );
 };
