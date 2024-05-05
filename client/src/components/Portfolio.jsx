c6c015da398b6e3b4c4e8ddc4285973e9287741e
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-05 13:27:54 -0400

    added initial layout to Portfolio.jsx

--- client/src/components/Portfolio.jsx
+++ client/src/components/Portfolio.jsx
@@ -2,13 +2,36 @@ const Portfolio = () => {
   return (
     <section>
       <h2>Portfolio</h2>
-      <p>Hello! Welcome to my Portfolio section</p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque iusto
-        autem ullam quibusdam quas earum dolorem odio aliquam, cupiditate, natus
-        temporibus molestiae illo, reiciendis corporis vero iste expedita nulla
-        labore.
-      </p>
+      <div>
+        <img src=""></img>
+        <a href="https://www.trustdnews.com">Pontual Social</a>
+        <a href="https://github.com/vpontual">GitHub Repo</a>
+      </div>
+      <div>
+        <img src=""></img>
+        <a href="https://www.trustdnews.com">Arlington Food Shelf</a>
+        <a href="https://github.com/vpontual">GitHub Repo</a>
+      </div>
+      <div>
+        <img src=""></img>
+        <a href="https://www.trustdnews.com">Mastodon</a>
+        <a href="https://github.com/vpontual">GitHub Repo</a>
+      </div>
+      <div>
+        <img src=""></img>
+        <a href="https://www.trustdnews.com">Lemmy</a>
+        <a href="https://github.com/vpontual">GitHub Repo</a>
+      </div>
+      <div>
+        <img src=""></img>
+        <a href="https://www.trustdnews.com">KBin</a>
+        <a href="https://github.com/vpontual">GitHub Repo</a>
+      </div>
+      <div>
+        <img src=""></img>
+        <a href="https://www.trustdnews.com">TrustDNEWS</a>
+        <a href="https://github.com/vpontual">GitHub Repo</a>
+      </div>
     </section>
   );
 };
