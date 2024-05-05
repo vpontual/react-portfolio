ed6e41c58bbce8b5cae1ad2a3ffb57410ee1181e
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-05 18:04:57 -0400

    working on fixig the image link in project.jsx

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -2,13 +2,9 @@ const Project = ({ project }) => {
   return (
     <div>
       <h3>{project.title}</h3>
-      <img src="path/to/project/image.jpg" alt={project.title} />
-      <a href={project.deployedLink} target="_blank" rel="noopener noreferrer">
-        Deployed App
-      </a>
-      <a href={project.githubLink} target="_blank" rel="noopener noreferrer">
-        GitHub Repository
-      </a>
+      {/* <img src=`/public/images/{project.title}".jpg` alt={project.title} /> */}
+      <a href={project.deployedLink}>Deployed App</a>
+      <a href={project.githubLink}>GitHub Repository</a>
     </div>
   );
 };
