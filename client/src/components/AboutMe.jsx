8961c8b64908361df60c6030c875b85ab8294ab8
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-05 12:19:21 -0400

    wrote the about me.jsx

---
+++ client/src/components/AboutMe.jsx
@@ -0,0 +1,11 @@
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="./client/public/images/whome.jpg" alt="Developer's Photo" />
+      <p>Hello! Welcome to my about me section</p>
+    </section>
+  );
+};
+
+export default AboutMe;
