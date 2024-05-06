341de7745d107dd08bafc97e29ebc48adbfbb8e9
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-06 19:31:25 -0400

    added global style to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
+import "../styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
