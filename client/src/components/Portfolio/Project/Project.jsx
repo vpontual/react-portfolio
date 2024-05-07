81a709f649591ca88472c528d926e7e4472bdd38
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-06 21:17:10 -0400

    added bootstrap card to projects

--- client/src/components/Portfolio/Project/Project.jsx
+++ client/src/components/Portfolio/Project/Project.jsx
@@ -4,6 +4,7 @@ import projectImage3 from "../../../images/project3.png";
 import projectImage4 from "../../../images/project4.png";
 import projectImage5 from "../../../images/project5.png";
 import projectImage6 from "../../../images/project6.png";
+import "./Project.module.css";
 
 const Project = ({ project }) => {
   const { title, deployedLink, githubLink } = project;
@@ -17,17 +18,26 @@ const Project = ({ project }) => {
   }[title];
 
   return (
-    <div>
-      <h3>{title}</h3>
-      {projectImage && (
-        <img
-          src={projectImage}
-          alt={title}
-          style={{ width: "400px", height: "auto" }}
-        />
-      )}
-      <a href={deployedLink}>Deployed App</a>
-      <a href={githubLink}>GitHub Repository</a>
+    <div className="col-md-4">
+      <div className="card">
+        {projectImage && (
+          <img
+            src={projectImage}
+            alt={title}
+            className="card-img-top"
+            style={{ width: "100%" }}
+          />
+        )}
+        <div className="card-body">
+          <h3 className="card-title">{title}</h3>
+          <a href={deployedLink} className="btn btn-primary">
+            Deployed App
+          </a>
+          <a href={githubLink} className="btn btn-secondary">
+            GitHub Repository
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
