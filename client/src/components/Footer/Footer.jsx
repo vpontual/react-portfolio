ef212f9d00820bd6a1f82f79e3ed0dfca067f681
veepee <66334129+vpontual@users.noreply.github.com>
2024-05-06 21:46:10 -0400

    added bootstrap to footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,27 +1,31 @@
-import { Link } from "react-router-dom";
-
 const Footer = () => {
   return (
-    <footer className="footer bg-dark text-white py-3">
+    <footer className="footer bg-dark text-center py-3">
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <Link
-              to={{ pathname: "https://github.com/vpontual" }}
-              target="_blank"
-              rel="noopener noreferrer"
+            <a
+              href="https://github.com/vpontual"
+              className="text-decoration-none"
             >
               GitHub
-            </Link>
+            </a>
           </div>
           <div className="col-md-4">
-            <Link
-              to={{ pathname: "https://linkedin.com/in/vitorpontual" }}
-              target="_blank"
-              rel="noopener noreferrer"
+            <a
+              href="https://linkedin.com/in/vitorpontual"
+              className="text-decoration-none"
             >
               LinkedIn
-            </Link>
+            </a>
+          </div>
+          <div className="col-md-4">
+            <a
+              href="https://linkedin.com/in/vitorpontual"
+              className="text-decoration-none"
+            >
+              Twitter
+            </a>
           </div>
         </div>
       </div>
